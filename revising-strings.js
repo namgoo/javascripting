0f7e16dccdf0a0ad63e9a6263e28b01dcf82e976
Nam Yoo <namgooy@gmail.com>
2017-06-21 23:47:16 +0000

    Initial commit with all my work so far

---
+++ revising-strings.js
@@ -0,0 +1,7 @@
+// var example = 'this example exists';
+// example = example.replace('exists','is awesome');
+// console.log(example)
+
+var pizza = 'pizza is alright';
+pizza = pizza.replace('alright','wonderful');
+console.log(pizza)
